f623b0c100b7c760f26e17770e8d242d6d14e98b
Jott4c <jrcba123@gmail.com>
2022-12-12 13:24:21 -0300

    ajustes deploy

--- _project/settings.py
+++ _project/settings.py
@@ -99,6 +99,13 @@ WSGI_APPLICATION = "_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -106,6 +113,7 @@ if DATABASE_URL:
     db_from_env = dj_database_url.config(
         default=DATABASE_URL, conn_max_age=500, ssl_require=True
     )
+    DATABASES["default"].update(db_from_env)
     DEBUG = False
 
 # Password validation

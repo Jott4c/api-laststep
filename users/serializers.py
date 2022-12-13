8550179f4a355d2591e1772ea9ab44f8098d2e98
Jott4c <jrcba123@gmail.com>
2022-12-13 20:50:55 -0300

    ajustes

--- users/serializers.py
+++ users/serializers.py
@@ -35,7 +35,7 @@ class UserSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
     age = serializers.DateField()
-    phone = serializers.CharField(max_length=11)
+    phone = serializers.CharField(max_length=14)
     url_image = serializers.CharField(max_length=270, required=False)
     type = serializers.ChoiceField(
         choices=Types.choices,
